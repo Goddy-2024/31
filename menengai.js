d5b3dcc2ebcf7e91227f901fd17049c5d422638f
Godswill <gaoajuoga@gmail.com>
2025-01-03 10:04:53 +0300

    Create menengai.js

---
+++ menengai.js
@@ -0,0 +1,39 @@
+const heal = document.querySelector('#healings');
+const conf1 = document.querySelector('#conf1');
+const conf2 = document.querySelector('#conf2'); 
+const ordain = document.querySelector('#ordain');
+const hdr = document.querySelector('header');
+const fconference = document.querySelector('.conference1');
+const ordination = document.querySelector('.ordination')
+const doc = document.querySelector('html');
+const rev = document.querySelector('#back');
+rev.onclick = reverse;
+conf1.onclick = conference1;
+ordain.onclick  = ordin;
+function reverse(){
+    conf1.style.display = 'block';
+    conf2.style.display = 'block';
+    heal.style.display = 'block';
+    ordain.style.display = 'block';
+    fconference.style.display = 'none';
+    ordination.style.display = 'none';
+    
+}
+function conference1(){
+   
+    conf1.style.display = 'none';
+    conf2.style.display = 'none';
+    heal.style.display = 'none';
+    hdr.style.position = 'relative';
+    ordain.style.display = 'none';
+    fconference.style.display = 'block';
+    document.querySelector('body').style.backgroundAttachment = 'fixed';
+}
+function ordin(){
+    conf1.style.display = 'none';
+    conf2.style.display = 'none';
+    heal.style.display = 'none';
+    hdr.style.position = 'relative';
+    ordain.style.display = 'none';
+    ordination.style.display = 'block';
+}
