d52f3fa2e7830e28bc3ce7db1eae393a296c4bdf
Godswill <gaoajuoga@gmail.com>
2025-01-03 17:00:24 +0300

    Update menengai.js

--- menengai.js
+++ menengai.js
@@ -4,9 +4,11 @@ const conf2 = document.querySelector('#conf2');
 const ordain = document.querySelector('#ordain');
 const hdr = document.querySelector('header');
 const fconference = document.querySelector('.conference1');
+const sconference = document.querySelector('#conference2');
 const ordination = document.querySelector('.ordination')
 const doc = document.querySelector('html');
 const rev = document.querySelector('#back');
+conf2.onclick = conference2;
 rev.onclick = reverse;
 conf1.onclick = conference1;
 ordain.onclick  = ordin;
@@ -16,8 +18,20 @@ function reverse(){
     heal.style.display = 'block';
     ordain.style.display = 'block';
     fconference.style.display = 'none';
+    sconference.style.display = 'none';
     ordination.style.display = 'none';
     
+}
+function conference2(){
+    conf1.style.display = 'none';
+    conf2.style.display = 'none';
+    heal.style.display = 'none';
+    hdr.style.position = 'relative';
+    ordain.style.display = 'none';
+    fconference.style.display = 'none';
+    sconference.style.display = 'block';
+
+    document.querySelector('body').style.backgroundAttachment = 'fixed';
 }
 function conference1(){
    
